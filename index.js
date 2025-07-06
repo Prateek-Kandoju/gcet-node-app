8dccd019b731a51819dd26a3391a083b58b978d5
Prateek-Kandoju <prateekkandoju@gmail.com>
2025-07-07 02:42:08 +0530

    test

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const MONGO_URI = process.env.MONGO_URI
 
 app.use("/users", userRouter);
 app.use("/products", productRouter);
-app.use("/orders",orderRouter)
+app.use("/orders", orderRouter);
 
 mongoose
   .connect(MONGO_URI)
